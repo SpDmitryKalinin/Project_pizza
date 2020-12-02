159214183477e7fc1deb24b4bf90491138c6cd47
Dmitry Kalinin <spkalinin97@gmail.com>
2020-12-02 17:49:56 +0300

    Исправлен баг слайдера

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,40 @@
 const bannerContainer = document.querySelector('.banner');
 const banner = document.querySelectorAll('.banner__image');
+let bannerArr = Array.from(banner);
 initBanner();
+addEventListeners();
 function initBanner(){
-    const bannerArr = Array.from(banner);
+    console.log(bannerArr);
     bannerArr.forEach(item =>{
         if(item.classList.contains('banner__active')){
-            initButtons(item, bannerArr)
+            initButtons(item);
         }
     });
 }
 
-function initButtons(item, bannerArr){
+function addEventListeners(){
+    bannerArr.forEach(bannerItem =>{
+        const buttons = Array.from(bannerItem.querySelectorAll('.banner__button'));
+        buttons.forEach(button =>{
+        if(button.classList.contains('banner__left-button')){
+            button.addEventListener('click',() => leftArrowFunction());
+        }
+        else{
+            button.addEventListener('click',() => rightArrowFunction());
+        }
+    });
+});
+}
+
+function initButtons(item){
     const buttons = Array.from(item.querySelectorAll('.banner__button'));
     buttons.forEach(button =>{
         button.removeAttribute('disabled');
-        if(button.classList.contains('banner__left-button')){
-            button.addEventListener('click',() => leftArrowFunction(bannerArr));
-        }
-        else{
-            button.addEventListener('click',() => rightArrowFunction(bannerArr));
-        }
     });
 }
 
-function leftArrowFunction(bannerArr){
+
+function leftArrowFunction(){
     for(let i=0; i<bannerArr.length; i++){
         if(bannerArr[i].classList.contains('banner__post')){
             bannerArr[i].classList.remove('banner__post');
@@ -32,7 +43,10 @@ function leftArrowFunction(bannerArr){
         }
         if(bannerArr[i].classList.contains('banner__active')){
             const buttons = Array.from(bannerArr[i].querySelectorAll('.banner__button'));
-            buttons.forEach(button =>{button.setAttribute("disabled", "disabled");});
+            buttons.forEach(button =>{
+                button.setAttribute("disabled", "disabled");
+                console.log(button);
+            });
             bannerArr[i].classList.remove('banner__active');
             bannerArr[i].classList.add('banner__post');
             continue;
